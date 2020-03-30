24d12a5c027af4f3de14e092b0a37fb3a4385823
leven_e <leven_going@163.com>
2020-03-30 17:11:57 +0800

    add base

--- src/router/index.js
+++ src/router/index.js
@@ -44,6 +44,7 @@ const routes = [
 ]
 
 const router = new VueRouter({
+  base: '/msystem/',
   routes
 })
 
--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,14 @@
 module.exports = {
+  publicPath: '/msystem/',
   devServer: {
+    host: '0.0.0.0',
     port: 8901
+    // proxy: {
+    //   '/api': {
+    //     target: 'http://120.77.25.155/',
+    //     changeOrigin: true
+    //   }
+    // }
   },
   css: {
     loaderOptions: {
