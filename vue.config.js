ca299a59704329e7e31f7147606cbc87845deb20
leven_e <leven_going@163.com>
2020-04-08 02:57:18 +0800

    change

--- vue.config.js
+++ vue.config.js
@@ -28,7 +28,7 @@ module.exports = {
             algorithm: 'gzip',
             test: /\.css$|\.ttf$|\.svg$|\.json$|\.js$/, // 匹配文件名
             threshold: 0, // 对超过0kb的数据压缩
-            deleteOriginalAssets: false // 是否删除源文件? true是||false否
+            deleteOriginalAssets: true // 是否删除源文件? true是||false否
           })
         ]
       }
